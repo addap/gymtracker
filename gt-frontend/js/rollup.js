1b265711de262f46fad152a4c68566e8997962f8
Adrian Dapprich <adrian@dappri.ch>
2023-02-06 17:48:49 +0900

    inject js values to add configuration options

---
+++ gt-frontend/js/rollup.js
@@ -0,0 +1,3 @@
+export const JS_BANNER = "引き締めたいカラダのために！";
+
+export const JS_MESSAGE_TIMEOUT = 2000;
--- gt-frontend/src/lib.rs
+++ gt-frontend/src/lib.rs
@@ -10,13 +10,23 @@ use dioxus_router::{Route, Router};
 use fermi::{use_init_atom_root, use_set};
 use futures_util::StreamExt;
 use gloo_timers::future::TimeoutFuture;
+use lazy_static::lazy_static;
+use wasm_bindgen::prelude::*;
 
 use components as c;
 pub use gt_core::APP_BASE;
 use messages::UIMessage;
 
-const BANNER: &'static str = "引き締めたいカラダのために！";
-const MESSAGE_TIMEOUT: u32 = 2_000;
+#[wasm_bindgen(module = "/js/rollup.js")]
+extern "C" {
+    static JS_BANNER: String;
+    static JS_MESSAGE_TIMEOUT: u32;
+}
+
+lazy_static! {
+    static ref BANNER: String = unsafe { JS_BANNER.clone() };
+    static ref MESSAGE_TIMEOUT: u32 = unsafe { JS_MESSAGE_TIMEOUT.clone() };
+}
 
 fn api_url(endpoint: &str) -> String {
     let base = web_sys::window().unwrap().origin();
@@ -39,7 +49,7 @@ pub fn app(cx: Scope) -> Element {
         async move {
             while let Some(ui_msg) = rx.next().await {
                 ui_messages.write().push_back(ui_msg);
-                TimeoutFuture::new(MESSAGE_TIMEOUT).await;
+                TimeoutFuture::new(*MESSAGE_TIMEOUT).await;
                 ui_messages.write().pop_front();
             }
         }
@@ -50,7 +60,7 @@ pub fn app(cx: Scope) -> Element {
             base_url: APP_BASE,
             c::Navbar {}
             c::Messages { ui_messages: ui_messages }
-            p { BANNER }
+            p { BANNER.clone() }
             Route { to: "/login", c::LoggedOut{ c::LoginPage { display_message: display_message } }}
             Route { to: "/register", c::LoggedOut {  c::RegisterPage { display_message: display_message }  }}
             Route { to: "/history", c::LoggedIn { c::HistoryPage {} }}
