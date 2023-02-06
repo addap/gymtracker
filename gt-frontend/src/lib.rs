730c588461df23aa69d4bf13675c377ffbfc81e4
Adrian Dapprich <adrian@dappri.ch>
2023-02-06 22:32:52 +0900

    better alert cleanup

--- gt-frontend/src/lib.rs
+++ gt-frontend/src/lib.rs
@@ -5,12 +5,15 @@ mod messages;
 use std::collections::VecDeque;
 
 use auth::{init_auth_token, ACTIVE_AUTH_TOKEN};
+use chrono::{DateTime, Duration, Utc};
 use dioxus::prelude::*;
 use dioxus_router::{Route, Router};
 use fermi::{use_init_atom_root, use_set};
 use futures_util::StreamExt;
 use gloo_timers::future::TimeoutFuture;
 use lazy_static::lazy_static;
+use log::info;
+use serde_json::to_vec_pretty;
 use wasm_bindgen::prelude::*;
 
 use components as c;
@@ -20,12 +23,12 @@ use messages::UIMessage;
 #[wasm_bindgen(module = "/js/rollup.js")]
 extern "C" {
     static JS_BANNER: String;
-    static JS_MESSAGE_TIMEOUT: u32;
+    static JS_MESSAGE_TIMEOUT: i32;
 }
 
 lazy_static! {
     static ref BANNER: String = unsafe { JS_BANNER.clone() };
-    static ref MESSAGE_TIMEOUT: u32 = unsafe { JS_MESSAGE_TIMEOUT.clone() };
+    static ref MESSAGE_TIMEOUT: i64 = unsafe { JS_MESSAGE_TIMEOUT.clone() as i64 };
 }
 
 fn api_url(endpoint: &str) -> String {
@@ -41,16 +44,29 @@ pub fn app(cx: Scope) -> Element {
 
     // Coroutine to display messages to the user.
     // Handler is passed to all components that need it.
-    // Messages are displayed for a couple of seconds, then removed.
+    // Messages are displayed for MESSAGE_TIMEOUT milliseconds, then removed.
     let ui_messages = use_ref(&cx, || VecDeque::new());
-    let display_message = use_coroutine(&cx, |mut rx: UnboundedReceiver<UIMessage>| {
+    let cleanup_messages = use_coroutine(&cx, |mut rx: UnboundedReceiver<DateTime<Utc>>| {
         to_owned![ui_messages];
+
+        async move {
+            while let Some(delete_at) = rx.next().await {
+                let now = Utc::now();
+                if let Ok(wait_ms) = (delete_at - now).num_milliseconds().try_into() {
+                    TimeoutFuture::new(wait_ms).await;
+                }
+                ui_messages.write().pop_front();
+            }
+        }
+    });
+    let display_message = use_coroutine(&cx, |mut rx: UnboundedReceiver<UIMessage>| {
+        to_owned![ui_messages, cleanup_messages];
 
         async move {
             while let Some(ui_msg) = rx.next().await {
                 ui_messages.write().push_back(ui_msg);
-                TimeoutFuture::new(*MESSAGE_TIMEOUT).await;
-                ui_messages.write().pop_front();
+                let delete_at = Utc::now() + Duration::milliseconds(*MESSAGE_TIMEOUT);
+                cleanup_messages.send(delete_at);
             }
         }
     });
