4a35d1b900aed44386e9f83bafe49ead4b029027
Adrian Dapprich <adrian@dappri.ch>
2023-02-06 16:29:57 +0900

    cleanup

--- gt-frontend/src/components/add_exercise_set.rs
+++ gt-frontend/src/components/add_exercise_set.rs
@@ -33,65 +33,78 @@ pub fn AddExerciseSetWeighted<'a>(cx: Scope<'a, AddExerciseProps<'a>>) -> Elemen
         );
 
     cx.render(rsx! {
-        input {
-            list: "w-exercise-names",
-            value: "{w_exercise_set_name}",
-            oninput: move |evt| w_exercise_set_name.set(evt.value.clone()),
-        }
-        datalist {
-            id: "w-exercise-names",
-            names_datalist
-        }
-        br {}
-        input {
-            r#type: "number",
-            value: "{w_exercise_set_weight}",
-            step: "any",
-            oninput: move |evt| {
-                if let Ok(v) = evt.value.parse() {
-                    w_exercise_set_weight.set(v)
-                }
+        div {
+            p { "Weighted Exercise Set" }
+            input {
+                list: "w-exercise-names-list",
+                value: "{w_exercise_set_name}",
+                placeholder: "exercise name",
+                oninput: move |evt| w_exercise_set_name.set(evt.value.clone()),
             }
-        }
-        br {}
-        input {
-            r#type: "number",
-            min: "1",
-            value: "{w_exercise_set_reps}",
-            oninput: move |evt| {
-                if let Ok(v) = evt.value.parse() {
-                    w_exercise_set_reps.set(v)
-                }
+            datalist {
+                id: "w-exercise-names-list",
+                names_datalist
             }
-        }
-        br {}
-        button {
-            onclick: move |_| cx.spawn({
-                to_owned![w_exercise_set_name, w_exercise_set_reps, w_exercise_set_weight, auth_token];
-                let fetch_names = cx.props.fetch_names.clone();
-                
-                async move {
-                    let client = reqwest::Client::new();
-                    
-                    if !w_exercise_set_name.current().is_empty() {
-                        let exs: models::ExerciseSet = (models::ExerciseSetWeighted {
-                            name: (*w_exercise_set_name.current()).clone(),
-                            reps: *w_exercise_set_reps.current(),
-                            weight: *w_exercise_set_weight.current(),
-                        }).into();
-
-                        let res = client.post(api_url("/exercise/set")).json(&exs).bearer_auth(auth_token.unwrap_or("".into()))
-                            .send().await;
-                        if let Err(ref e) = res {
-                            info!("{}", e);
-                            return;
-                        }
-
-                        fetch_names.send(c::main_page::FetchNames);
+            br {}
+            input {
+                id: "w-exercise-set-weight",
+                r#type: "number",
+                value: "{w_exercise_set_weight}",
+                step: "any",
+                oninput: move |evt| {
+                    if let Ok(v) = evt.value.parse() {
+                        w_exercise_set_weight.set(v)
                     }
                 }
-            }),
-            "+"
+            }
+            label {
+                r#for: "w-exercise-set-weight",
+                " kg"
+            }
+            br {}
+            input {
+                id: "w-exercise-set-reps",
+                r#type: "number",
+                min: "1",
+                value: "{w_exercise_set_reps}",
+                oninput: move |evt| {
+                    if let Ok(v) = evt.value.parse() {
+                        w_exercise_set_reps.set(v)
+                    }
+                }
+            }
+            label {
+                r#for: "w-exercise-set-reps",
+                " reps"
+            }
+            br {}
+            button {
+                onclick: move |_| cx.spawn({
+                    to_owned![w_exercise_set_name, w_exercise_set_reps, w_exercise_set_weight, auth_token];
+                    let fetch_names = cx.props.fetch_names.clone();
+                    
+                    async move {
+                        let client = reqwest::Client::new();
+                        
+                        if !w_exercise_set_name.current().is_empty() {
+                            let exs: models::ExerciseSet = (models::ExerciseSetWeighted {
+                                name: (*w_exercise_set_name.current()).clone(),
+                                reps: *w_exercise_set_reps.current(),
+                                weight: *w_exercise_set_weight.current(),
+                            }).into();
+
+                            let res = client.post(api_url("/exercise/set")).json(&exs).bearer_auth(auth_token.unwrap_or("".into()))
+                                .send().await;
+                            if let Err(ref e) = res {
+                                info!("{}", e);
+                                return;
+                            }
+
+                            fetch_names.send(c::main_page::FetchNames);
+                    }
+                }),
+                "+"
+            }
         }
     })
 }
@@ -110,53 +123,62 @@ pub fn AddExerciseSetBodyweight<'a>(cx: Scope<'a, AddExerciseProps<'a>>) -> Elem
         );
 
     cx.render(rsx! {
-        input {
-            list: "b-exercise-names",
-            value: "{w_exercise_set_name}",
-            oninput: move |evt| w_exercise_set_name.set(evt.value.clone()),
-        }
-        datalist {
-            id: "b-exercise-names",
-            names_datalist
-        }
-        br {}
-        input {
-            r#type: "number",
-            min: "1",
-            value: "{w_exercise_set_reps}",
-            oninput: move |evt| {
-                if let Ok(v) = evt.value.parse() {
-                    w_exercise_set_reps.set(v)
-                }
+        div {
+            p { "Bodyweight Exercise Set" }
+            input {
+                list: "b-exercise-names-list",
+                value: "{w_exercise_set_name}",
+                placeholder: "exercise name",
+                oninput: move |evt| w_exercise_set_name.set(evt.value.clone()),
             }
-        }
-        br {}
-        button {
-            onclick: move |_| cx.spawn({
-                to_owned![w_exercise_set_name, w_exercise_set_reps, auth_token];
-                let fetch_names = cx.props.fetch_names.clone();
-                
-                async move {
-                    let client = reqwest::Client::new();
-                    
-                    if !w_exercise_set_name.current().is_empty() {
-                        let exs: models::ExerciseSet = (models::ExerciseSetBodyweight {
-                            name: (*w_exercise_set_name.current()).clone(),
-                            reps: *w_exercise_set_reps.current(),
-                        }).into();
-
-                        let res = client.post(api_url("/exercise/set")).json(&exs).bearer_auth(auth_token.unwrap_or("".into()))
-                            .send().await;
-                        if let Err(ref e) = res {
-                            info!("{}", e);
-                            return;
-                        }
-
-                        fetch_names.send(c::main_page::FetchNames);
+            datalist {
+                id: "b-exercise-names-list",
+                names_datalist
+            }
+            br {}
+            input {
+                id: "b-exercise-set-reps",
+                r#type: "number",
+                min: "1",
+                value: "{w_exercise_set_reps}",
+                oninput: move |evt| {
+                    if let Ok(v) = evt.value.parse() {
+                        w_exercise_set_reps.set(v)
                     }
                 }
-            }),
-            "+"
+            }
+            label {
+                r#for: "b-exercise-set-reps",
+                " reps"
+            }
+            br {}
+            button {
+                onclick: move |_| cx.spawn({
+                    to_owned![w_exercise_set_name, w_exercise_set_reps, auth_token];
+                    let fetch_names = cx.props.fetch_names.clone();
+                    
+                    async move {
+                        let client = reqwest::Client::new();
+                        
+                        if !w_exercise_set_name.current().is_empty() {
+                            let exs: models::ExerciseSet = (models::ExerciseSetBodyweight {
+                                name: (*w_exercise_set_name.current()).clone(),
+                                reps: *w_exercise_set_reps.current(),
+                            }).into();
+
+                            let res = client.post(api_url("/exercise/set")).json(&exs).bearer_auth(auth_token.unwrap_or("".into()))
+                                .send().await;
+                            if let Err(ref e) = res {
+                                info!("{}", e);
+                                return;
+                            }
+
+                            fetch_names.send(c::main_page::FetchNames);
+                        }
+                    }
+                }),
+                "+"
+            }
         }
     })
 }
---
+++ gt-frontend/src/components/messages.rs
@@ -0,0 +1,28 @@
+#![allow(non_snake_case)]
+use std::collections::VecDeque;
+
+use dioxus::prelude::*;
+
+use crate::{UIMessage, UIMessageType};
+
+#[inline_props]
+pub fn Messages<'a>(cx: Scope, ui_messages: &'a UseRef<VecDeque<UIMessage>>) -> Element<'a> {
+    // let message_items = ;
+
+    cx.render(rsx! {
+        ul {
+            ui_messages.read().iter().map(|ui_message| {
+                match ui_message.r#type {
+                    UIMessageType::Info =>
+                        rsx! {
+                            li { ui_message.message.clone() }
+                        },
+                    UIMessageType::Error =>
+                        rsx! {
+                            li { format!("Error: {}", ui_message.message) }
+                        },
+                }
+            })
+        }
+    })
+}
