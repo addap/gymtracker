b67de337750b479704c5470ebb9b3689aac808c2
Adrian Dapprich <adrian@dappri.ch>
2023-02-19 16:34:30 +0900

    automatically deploy to server

--- release.sh
+++ release.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
 docker build -t registry.gitlab.com/addapp/gymtracker . && docker push registry.gitlab.com/addapp/gymtracker
+
+if [[ "$1" == "--deploy" ]]; then
+	echo "deploying"
+	ssh netcup "cd dockerfiles/gymtracker && docker-compose down && docker-compose pull web && docker-compose up -d"
+fi
